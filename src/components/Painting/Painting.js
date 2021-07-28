f11f26bdf865b3de4a6148ad935e0cbbfde23de1
Vitaliy-Yakovlev <vitaliy-yakovlev@ukr.net>
2021-07-28 13:31:03 +0300

    add css

--- src/components/Painting/Painting.js
+++ src/components/Painting/Painting.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import defaultImages from './default.jpg';
-import styles from './Painting.module.css';
+import s from './Painting.module.css';
 
 const Painting = ({
   imageUrl = defaultImages,
@@ -10,16 +10,18 @@ const Painting = ({
   price,
   quantity,
 }) => (
-  <div className={styles.container}>
+  <div className={s.container}>
     <img src={imageUrl ?? defaultImages} alt={title} width="480" />
-    <h2>{title}</h2>
-    <p>
+    <h2 className={s.title}>{title}</h2>
+    <p className={s.text}>
       Автор: <a href={profileUrl}>{authorName}</a>
     </p>
-    <p>Цена:{price} кредитов</p>
+    <p className={s.text}>Цена:{price} кредитов</p>
 
-    <p>Доступность: {quantity < 10 ? 'Доступно' : 'Заканчиваются'}</p>
-    <button type="button" className={styles.btn}>
+    <p className={s.text}>
+      Доступность: {quantity < 10 ? 'Доступно' : 'Заканчиваются'}
+    </p>
+    <button type="button" className={s.btn}>
       Добавить в корзину
     </button>
   </div>
